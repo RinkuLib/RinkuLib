d636f339bc37650c54f9db5133e6985a7cca8fa3
adamtherrienmarois <ADAMTHERRIENMAROIS@OUTLOOK.COm>
2026-03-09 16:10:33 -0400

    Clean up demo

---
+++ RinkuDemo/Controller.cs
@@ -0,0 +1,88 @@
+﻿using System.Data.Common;
+using RinkuLib.Commands;
+using RinkuLib.DbRegister;
+using RinkuLib.Queries;
+
+namespace RinkuDemo;
+
+public delegate void SetID<T, TId>(ref T item, TId ID);
+public delegate TId GetID<T, TId>(ref T item);
+
+public class Controller<T, TId> where T : IHasID<TId> {
+    public string[] ActionsOnGetOne;
+    public QueryCommand InsertQuery;
+    public QueryCommand SelectQuery;
+    public QueryCommand UpdateQuery;
+    public QueryCommand DeleteQuery;
+    public string Name;
+    public Controller(IConfiguration config, string key) {
+        InsertQuery = new(config[$"SQLStrings:{key}:Insert"] ?? throw new Exception($"{key} : Insert does not exist"));
+        SelectQuery = new(config[$"SQLStrings:{key}:Select"] ?? throw new Exception($"{key} : Select does not exist"));
+        UpdateQuery = new(config[$"SQLStrings:{key}:Update"] ?? throw new Exception($"{key} : Update does not exist"));
+        DeleteQuery = new(config[$"SQLStrings:{key}:Delete"] ?? throw new Exception($"{key} : Delete does not exist"));
+        ActionsOnGetOne = config.GetSection($"SQLStrings:{key}:ActionsOnGetOne").Get<string[]>() ?? [];
+        this.Name = key;
+        if (!SelectQuery.Mapper.ContainsKey("@ID"))
+            throw new Exception($"The select should have a @ID variable");
+        if (!DeleteQuery.Mapper.ContainsKey("@ID"))
+            throw new Exception($"The delete should have a @ID variable");
+        /* To change things up, we check when binding the ID in the update
+        if (!UpdateQuery.Mapper.ContainsKey("@ID"))
+            throw new Exception($"The update should have a @ID variable");*/
+    }
+    public async Task<T> Create(T item) {
+        using var db = Registry.GetConnection();
+        var ID = await InsertQuery.ExecuteScalarAsync<TId>(db, item);
+        item.ID = ID;
+        return item;
+    }
+    public async Task<bool> Delete(TId id) {
+        using var db = Registry.GetConnection();
+        db.Open();
+        using var tx = db.BeginTransaction();
+        try {
+            var rows = await DeleteQuery.ExecuteAsync(db, new { ID = id }, tx);
+            tx.Commit();
+            return rows > 0;
+        }
+        catch { tx.Rollback(); throw; }
+    }
+    public IAsyncEnumerable<T> GetAll(HttpContext context) {
+        using var db = Registry.GetConnection();
+        return GetAll(db, context);
+    }
+    private IAsyncEnumerable<T> GetAll(DbConnection db, HttpContext context) {
+        var b = SelectQuery.StartBuilder();
+        Registry.FillQueryBuilder(context, b, context.Request.Query, out var actions);
+        if (actions.Length <= 0)
+            return b.QueryAllAsync<T>(db);
+        return GetAll(db, b, actions);
+    }
+    private static async IAsyncEnumerable<T> GetAll(DbConnection db, QueryBuilder b, string[] actions) {
+        if (typeof(T).IsValueType) {
+            foreach (var item in await b.QueryAllBufferedAsync<T>(db).ExecuteDBActionsAsync(db, actions).ConfigureAwait(false))
+                yield return item;
+            yield break;
+        }
+        await foreach (var item in b.QueryAllAsync<T>(db)) {
+            foreach (var actionName in actions)
+                await item.ExecuteDBActionAsync(db, actionName).ConfigureAwait(false);
+            yield return item;
+        }
+    }
+    public async Task<T?> GetOne(TId id) {
+        using var db = Registry.GetConnection();
+        var task = SelectQuery.QueryOneAsync<T>(db, new { ID = id });
+        if (ActionsOnGetOne.Length <= 0)
+            return await task;
+        return await task.ExecuteDBActionsAsync(db, ActionsOnGetOne);
+    }
+    public async Task<bool> Update(TId id, HttpContext context) {
+        using var db = Registry.GetConnection();
+        var b = UpdateQuery.StartBuilder();
+        if (!b.Use("@ID", id))
+            throw new Exception("Could not bind the ID");
+        Registry.FillQueryBuilder(context, b, context.Request.Form, out var actions);
+        return await b.ExecuteAsync(db) > 0;
+    }
+}
--- RinkuDemo/Types.cs
+++ RinkuDemo/Types.cs
@@ -5,26 +5,33 @@ using RinkuLib.DbRegister;
 
 namespace RinkuDemo;
 
-public record Artist(int ID, string Name) : IDbReadable {
-    [PopulateList("ID", "SELECT {0}AlbumId AS ID, Title FROM albums WHERE ArtistId {1}", "ArtistId")]
+public record Artist(int ID, string Name) : IHasID<int> {
+    public int ID { get; set; } = ID;
+    [ToMany("ID", "SELECT {0}AlbumId AS ID, Title FROM albums WHERE ArtistId {1} ORDER BY ArtistId", "ArtistId")]
     public List<Album> Albums { get; set; } = [];
 }
 
-public record Album(int ID, string Title, Artist? Artist = null) : IDbReadable {
+public record Album(int ID, string Title, Artist? Artist = null) : IHasID<int> {
+    public int ID { get; set; } = ID;
     public int? ArtistID => Artist?.ID;
 
-    [PopulateList("ID", "SELECT {0}t.TrackId AS ID, t.Name, t.Composer, t.Milliseconds, t.Bytes, t.UnitPrice, t.MediaTypeId AS MediaTypeID, mt.Name AS MediaTypeName, t.GenreId AS GenreID, g.Name AS GenreName FROM tracks t INNER JOIN media_types mt ON t.MediaTypeId = mt.MediaTypeId INNER JOIN genres g ON t.GenreId = g.GenreId WHERE t.AlbumId {1}", "t.AlbumId")]
+    [ToMany("ID", "SELECT {0}t.TrackId AS ID, t.Name, t.Composer, t.Milliseconds, t.Bytes, t.UnitPrice, t.MediaTypeId AS MediaTypeID, mt.Name AS MediaTypeName, t.GenreId AS GenreID, g.Name AS GenreName FROM tracks t INNER JOIN media_types mt ON t.MediaTypeId = mt.MediaTypeId INNER JOIN genres g ON t.GenreId = g.GenreId WHERE t.AlbumId {1} ORDER BY t.AlbumId", "t.AlbumId")]
     public List<Track> Tracks { get; set; } = [];
 }
 
-public record Track(int ID, string Name, string Composer, int Milliseconds, int Bytes, decimal UnitPrice, Album? Album = null, Reference? MediaType = null, KeyValuePair<int, string>? Genre = null) : IDbReadable {
+public record Track(int ID, string Name, string Composer, int Milliseconds, int Bytes, decimal UnitPrice, Album? Album = null, Reference? MediaType = null, KeyValuePair<int, string>? Genre = null) : IHasID<int> {
+    public int ID { get; set; } = ID;
     public int? AlbumID => Album?.ID;
     public int? ArtistID => Album?.Artist?.ID;
     public int? MediaTypeID => MediaType?.ID;
     public int? GenreID => Genre?.Key;
 }
 
-public record struct Reference([Alt("Key")][InvalidOnNull]int ID, [Alt("Name")]string Value) : IDbReadable;
+public record struct Reference([Alt("Key")][InvalidOnNull]int ID, [Alt("Name")]string Value) : IHasID<int> {
+    public int ID { get; set; } = ID;
+    public static implicit operator Reference(KeyValuePair<int, string> Kvp) => new(Kvp.Key, Kvp.Value);
+    public static implicit operator KeyValuePair<int, string>(Reference Ref) => new(Ref.ID, Ref.Value);
+}
 
 public record Employee([InvalidOnNull]int ID, string FirstName, string LastName,
     [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] string? Title,
@@ -39,25 +46,30 @@ public record Employee([InvalidOnNull]int ID, string FirstName, string LastName,
     [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] string? Fax = null,
     [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] string? Email = null,
     Employee? Manager = null
-) : IDbReadable {
+) : IHasID<int> {
+    public int ID { get; set; } = ID;
     public int? ReportsTo => Manager?.ID;
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-    [PopulateList("ID", "SELECT {0}EmployeeId AS ID, FirstName, LastName, Title, BirthDate, HireDate, Address, City, State, Country, PostalCode, Phone, Fax, Email FROM employees WHERE ReportsTo {1}", "ReportsTo")]
+    [ToMany("ID", "SELECT {0}EmployeeId AS ID, FirstName, LastName, Title, BirthDate, HireDate, Address, City, State, Country, PostalCode, Phone, Fax, Email FROM employees WHERE ReportsTo {1} ORDER BY ReportsTo", "ReportsTo")]
     public List<Employee> ManagingEmployees { get; set; } = [];
 }
 
-public record Customer([InvalidOnNull]int ID, [NotNull]string FirstName, [NotNull]string LastName, string? Company, string? Address, string? City, string? State, string? Country, string? PostalCode, string? Phone, string? Fax, string? Email, Employee? SupportRep = null) : IDbReadable {
+public record Customer([InvalidOnNull]int ID, [NotNull]string FirstName, [NotNull]string LastName, string? Company, string? Address, string? City, string? State, string? Country, string? PostalCode, string? Phone, string? Fax, string? Email, Employee? SupportRep = null) : IHasID<int> {
+    public int ID { get; set; } = ID;
     public int? SupportRepID => SupportRep?.ID;
-    [PopulateList("ID", "SELECT {0}InvoiceId AS ID, InvoiceDate, Total, BillingAddress, BillingCity, BillingState, BillingCountry, BillingPostalCode FROM invoices WHERE CustomerId {1}", "CustomerId")]
+    [ToMany("ID", "SELECT {0}InvoiceId AS ID, InvoiceDate, Total, BillingAddress, BillingCity, BillingState, BillingCountry, BillingPostalCode FROM invoices WHERE CustomerId {1} ORDER BY CustomerId", "CustomerId")]
     public List<Invoice> Invoices { get; set; } = [];
 }
 
-public record Invoice(int ID, DateTime InvoiceDate, decimal Total, string BillingAddress, string BillingCity, string BillingState, string BillingCountry, string BillingPostalCode, Customer? Customer = null) : IDbReadable {
+public record Invoice(int ID, DateTime InvoiceDate, decimal Total, string BillingAddress, string BillingCity, string BillingState, string BillingCountry, string BillingPostalCode, Customer? Customer = null) : IHasID<int> {
+    public int ID { get; set; } = ID;
     public int? CustomerID => Customer?.ID;
-    [PopulateList("ID", "SELECT {0}ii.InvoiceLineId AS ID, ii.UnitPrice, ii.Quantity, ii.InvoiceId AS InvoiceID, ii.TrackId AS TrackID, t.Name AS TrackName FROM invoice_items ii INNER JOIN tracks t ON ii.TrackId = t.TrackId WHERE ii.InvoiceId {1}", "ii.InvoiceId")]
+    [ToMany("ID", "SELECT {0}ii.InvoiceLineId AS ID, ii.UnitPrice, ii.Quantity, ii.InvoiceId AS InvoiceID, ii.TrackId AS TrackID, t.Name AS TrackName FROM invoice_items ii INNER JOIN tracks t ON ii.TrackId = t.TrackId WHERE ii.InvoiceId {1} ORDER BY ii.InvoiceId", "ii.InvoiceId")]
     public List<InvoiceLine> Lines { get; set; } = [];
 }
 
-public record InvoiceLine(int ID, decimal UnitPrice, int Quantity, int InvoiceID, int TrackID, string TrackName) : IDbReadable;
+public record InvoiceLine(int ID, decimal UnitPrice, int Quantity, int InvoiceID, int TrackID, string TrackName) : IHasID<int> {
+    public int ID { get; set; } = ID;
+}
 
